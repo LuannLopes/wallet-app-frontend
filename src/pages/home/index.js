38cb06a7dcaee7d59824d5e35576ebcf341d6217
LuannLopes <luanlopesofc@gmail.com>
2023-11-18 19:02:42 -0300

    fix: a bug that was not rendering data in the table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -59,7 +59,6 @@ const renderFinancesList = (data) => {
 
   data.map((item) => {
     const tableRow = document.createElement("tr");
-    tableRow.className = "mt smaller";
 
     // title
     const titleTd = document.createElement("td");
@@ -227,11 +226,13 @@ const onLoadUserInfo = () => {
   const navbarUserInfo = document.getElementById("navbar-user-container");
   const navbarUserAvatar = document.getElementById("navbar-user-avatar");
 
+  // add user email
   const emailElement = document.createElement("p");
   const emailText = document.createTextNode(email);
   emailElement.appendChild(emailText);
   navbarUserInfo.appendChild(emailElement);
 
+  // add logout link
   const logoutElement = document.createElement("a");
   logoutElement.onclick = () => onLogout();
   logoutElement.style.cursor = "pointer";
@@ -239,6 +240,7 @@ const onLoadUserInfo = () => {
   logoutElement.appendChild(logoutText);
   navbarUserInfo.appendChild(logoutElement);
 
+  // add user first letter inside avatar
   const nameElement = document.createElement("h3");
   const nameText = document.createTextNode(name.charAt(0));
   nameElement.appendChild(nameText);
